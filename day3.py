65b1f389afc0e7a433ada1939c5d0800ff894402
Sander De Bleecker <sanderdebleecker82@gmail.com>
2024-12-03 21:44:24 +0100

    feat(day3): part 1 get sum of multiplications parsed out of the input file

--- day3.py
+++ day3.py
@@ -4,10 +4,22 @@ import regex as re
 def main():
     '''using https://regex101.com/'''
 
-    pattern = "mul\([0-9]{1,3},[0-9]{1,3}\)"
-    text = "mul(123,1)ezzefzefz"
-    matches = re.findall(pattern, text)
-    print(matches)
+    multiplication_pattern = "mul\([0-9]{1,3},[0-9]{1,3}\)"
+    with open('day3_input.txt') as file:
+        text = file.read()
+    multiplications = re.findall(multiplication_pattern, text)
+    sum_of_multiplications = get_sum_of_multiplications_functions(multiplications)
+    print(sum_of_multiplications)
+
+
+def get_sum_of_multiplications_functions(multiplications):
+    sum = 0
+    numbers_in_function_pattern = "[0-9]{1,3}"
+    for multiplication in multiplications:
+        numbers = re.findall(numbers_in_function_pattern, multiplication)
+        multiplication_result = int(numbers[0])*int(numbers[1])
+        sum += multiplication_result
+    return sum
 
 
 if __name__ == "__main__":
