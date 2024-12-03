c0b335507c1275badd3eae00bb8c399557cd371a
Sander De Bleecker <sanderdebleecker82@gmail.com>
2024-12-03 22:12:37 +0100

    feat(day3): part 3 read "do" and don't" instructions to toggle on or off the parsing of multiplications

--- day3.py
+++ day3.py
@@ -2,23 +2,36 @@ import regex as re
 
 
 def main():
-    '''using https://regex101.com/'''
+    '''calculates a sum via instructions read from a gibberish file'''
 
-    multiplication_pattern = "mul\([0-9]{1,3},[0-9]{1,3}\)"
+    # regex patterns tested using https://regex101.com/
+    multiplication_pattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
+    do_pattern = r"do\(\)"
+    dont_pattern = r"don\'t\(\)"
+    parseable_functions_pattern = f'{multiplication_pattern}|{do_pattern}|{dont_pattern}'
+
     with open('day3_input.txt') as file:
         text = file.read()
-    multiplications = re.findall(multiplication_pattern, text)
-    sum_of_multiplications = get_sum_of_multiplications_functions(multiplications)
+    functions = re.findall(parseable_functions_pattern, text)
+    sum_of_multiplications = get_sum_of_multiplications_functions(functions, multiplication_pattern, do_pattern, dont_pattern)
     print(sum_of_multiplications)
 
 
-def get_sum_of_multiplications_functions(multiplications):
+def get_sum_of_multiplications_functions(functions, multiplication_pattern, do_pattern, dont_pattern):
     sum = 0
     numbers_in_function_pattern = "[0-9]{1,3}"
-    for multiplication in multiplications:
-        numbers = re.findall(numbers_in_function_pattern, multiplication)
-        multiplication_result = int(numbers[0])*int(numbers[1])
-        sum += multiplication_result
+    may_parse_multiplication = True
+    for parsable_function in functions:
+        if re.match(do_pattern, parsable_function):
+            may_parse_multiplication = True
+            continue
+        elif re.match(dont_pattern, parsable_function):
+            may_parse_multiplication = False
+            continue
+        elif re.match(multiplication_pattern, parsable_function) and may_parse_multiplication:
+            numbers = re.findall(numbers_in_function_pattern, parsable_function)
+            multiplication_result = int(numbers[0])*int(numbers[1])
+            sum += multiplication_result
     return sum
 
 
